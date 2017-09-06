76ea5e607cbe954f607b3ca307d9724450389456
mrox <tuyen.appdev@gmail.com>
2017-09-06 16:15:18 +0700

    v

--- src/app/Services/TrieuChung.service.ts
+++ src/app/Services/TrieuChung.service.ts
@@ -24,13 +24,13 @@ export class TrieuChungService {
     }
 
     BenhFromTrieuChung(icds): Observable<any> {
-        const body = new FormData();
-        body.append('IdsTrieuChung', icds);
+        // const body = new FormData();
+        // body.append('ids', icds);
         // const headers = new Headers();
         // headers.append('Content-Type', 'application/x-www-form-urlencoded');
-        const options = new RequestOptions({  });
+        // const options = new RequestOptions({ headers: headers });
 
-        return this.http.post(`${environment.apiUrl}CSDLYT/Benh_From_TCIDS`, body, options)
+        return this.http.post(`http://localhost:5001/api/CSDLYT/Benh_From_TCIDS`, icds, null)
             // ...and calling .json() on the response to return data
             .map((res: Response) => res.json())
             // ...errors if any
